e7c1dac921753ae60b3193cfa295d73271cb8f29
Facu <chiarini.facundo@gmail.com>
2022-04-02 22:23:01 -0300

    Back
    BAckendd

---
+++ subscription-handle/src/api/suscription/controllers/suscription.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  suscription controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::suscription.suscription');
---
+++ subscription-handle/src/api/suscription/routes/suscription.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * suscription router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::suscription.suscription');
